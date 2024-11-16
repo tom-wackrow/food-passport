f0a1bef3a3958a68aa2f234594fc2b9b6fc8f623
Tom Foster <trfoster1794@gmail.com>
2024-11-16 17:38:59 +0000

    added some basic methods to services

--- src/main/java/uk/ac/sheffield/foodpassport/service/UserService.java
+++ src/main/java/uk/ac/sheffield/foodpassport/service/UserService.java
@@ -2,6 +2,13 @@ package uk.ac.sheffield.foodpassport.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import uk.ac.sheffield.foodpassport.model.Meal;
+import uk.ac.sheffield.foodpassport.repository.MealRepository;
+import uk.ac.sheffield.foodpassport.repository.UserRepository;
+import uk.ac.sheffield.foodpassport.model.User;
+
+import java.util.List;
+import java.util.Optional;
 
 import uk.ac.sheffield.foodpassport.model.User;
 import uk.ac.sheffield.foodpassport.repository.UserRepository;
@@ -10,10 +17,34 @@ import uk.ac.sheffield.foodpassport.repository.UserRepository;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MealRepository mealRepository;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, MealRepository mealRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.mealRepository = mealRepository;
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findById(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User addMealToUser(String username, Long mealId) {
+        Optional<User> userOptional = userRepository.findById(username);
+        Optional<Meal> mealOptional = mealRepository.findById(mealId);
+
+        if (userOptional.isPresent() && mealOptional.isPresent()) {
+            User user = userOptional.get();
+            Meal meal = mealOptional.get();
+
+            user.getMeals().add(meal);
+            return userRepository.save(user);
+        }
+        return null;
     }
 
     public User register(User user) throws Exception {
