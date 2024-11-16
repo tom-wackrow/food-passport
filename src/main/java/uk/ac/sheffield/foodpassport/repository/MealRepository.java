0581b983b43d7158ded13784fed54c8ed6785a5f
Tom Foster <trfoster1794@gmail.com>
2024-11-16 17:38:59 +0000

    added some basic methods to services

--- src/main/java/uk/ac/sheffield/foodpassport/repository/MealRepository.java
+++ src/main/java/uk/ac/sheffield/foodpassport/repository/MealRepository.java
@@ -2,7 +2,14 @@ package uk.ac.sheffield.foodpassport.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uk.ac.sheffield.foodpassport.model.Meal;
 
+import java.util.List;
+
 public interface MealRepository extends JpaRepository<Meal, Long> {
+
+    @Query("SELECT m FROM Meal m WHERE m.owner = :userName")
+    List<Meal> findMealsByUsername(@Param("userName") String userName);
 }
