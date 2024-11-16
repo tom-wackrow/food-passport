34e9f469cecea9857a719153cc25307c8494809a
Tom Wackrow <wackrowtom@gmail.com>
2024-11-16 20:41:35 +0000

    add some routes and fix disgusting css imports

--- src/main/java/uk/ac/sheffield/foodpassport/SecurityConfig.java
+++ src/main/java/uk/ac/sheffield/foodpassport/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/register", "/login").permitAll()
+                        .requestMatchers("/register", "/login", "/js/**", "/css/**", "/image/**", "/**").permitAll()
                         .requestMatchers("/").authenticated()
                         .anyRequest().authenticated())
                 .formLogin((form) -> form
---
+++ src/main/java/uk/ac/sheffield/foodpassport/controller/PagesController.java
@@ -0,0 +1,31 @@
+package uk.ac.sheffield.foodpassport.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagesController {
+    @GetMapping("/")
+    public String index(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null) {
+            return "login";
+        }
+        return "general";
+    }
+
+    @GetMapping("/map")
+    public String showMapPage(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null) {
+            return "login";
+        }
+
+        return "map";
+    }
+}
