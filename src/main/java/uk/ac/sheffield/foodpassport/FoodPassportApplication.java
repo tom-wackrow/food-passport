9dc8249ccbc3042a9427a5fa7b6e0e4b13088e29
foamtoaster <trfoster1794@gmail.com>
2024-11-16 13:55:48 +0000

    initial commit

---
+++ src/main/java/uk/ac/sheffield/foodpassport/FoodPassportApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.sheffield.foodpassport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPassportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodPassportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/ac/sheffield/foodpassport/FoodPassportApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.sheffield.foodpassport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPassportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
