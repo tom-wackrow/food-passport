f16763706659f1bce0ed7b14bf7fc1d8d9166214
yejinkang <yejink0105@gmail.com>
2024-11-17 02:56:03 +0000

    general page updated

--- src/main/resources/static/js/general.js
+++ src/main/resources/static/js/general.js
@@ -6,6 +6,7 @@ const addMealButton = document.getElementById('add');
 // Function to show the form and hide the navbar
 toggleButton.addEventListener('click', () => {
     navbar.style.display = 'none'; // Hide navbar
+    document.getElementById("legends").textContent = "Add new dish"; // Update text
     form.classList.remove('hidden'); // Show form
 });
 
@@ -13,5 +14,6 @@ toggleButton.addEventListener('click', () => {
 addMealButton.addEventListener('click', (event) => {
     event.preventDefault(); // Prevent form submission from refreshing the page
     form.classList.add('hidden'); // Hide form
+    document.getElementById("legends").textContent = "Legends";
     navbar.style.display = 'flex'; // Show navbar again
 });
